30432dd2e1c744237adfc9702116030b1f30691b
Ahmed Siddique <siddiqua@greenitglobe.com>
2019-09-24 16:17:23 +0400

    Update package

---
+++ package.py
@@ -0,0 +1,53 @@
+from Jumpscale import j
+
+class Package(j.baseclasses.threebot_package):
+    """
+    to start need to run 
+    kosmos -p "j.tools.threebot_packages.get('3bot_org',giturl='https://github.com/threefoldtech/www_3bot_org.git',branch='master')"
+    kosmos -p "j.servers.threebot.default.start(web=True, ssl=False)"
+    """
+    def _init(self, **kwargs):
+        self.branch = kwargs["package"].branch or "master"
+        self.3bot_org = "https://github.com/threefoldtech/www_3bot_org.git"
+
+    def prepare(self):
+        """
+        called when the 3bot starts
+        :return:
+        """
+        server = self.openresty
+        server.install(reset=True)
+        server.configure()
+        website = server.websites.get("3bot_org")
+        website.ssl = False
+        website.port = 80
+        locations = website.locations.get("3bot_org")
+        static_location = locations.locations_static.new()
+        static_location.name = "static"
+        static_location.path_url = "/"
+        path = j.clients.git.getContentPathFromURLorPath(self.3bot_org, branch=self.branch, pull=True)
+        static_location.path_location = path
+        static_location.use_jumpscale_weblibs = True
+        website.path = path
+        locations.configure()
+        website.configure()
+
+    def start(self):
+        """
+        called when the 3bot starts
+        :return:
+        """
+        self.prepare()
+    def stop(self):
+        """
+        called when the 3bot stops
+        :return:
+        """
+        pass
+
+    def uninstall(self):
+        """
+        called when the package is no longer needed and will be removed from the threebot
+        :return:
+        """
+        pas
