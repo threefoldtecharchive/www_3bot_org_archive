a04f2ae6ecec40e15c957218f6b96e53e17e21b2
andrewayoub <andrew.a.mikhaeel@gmail.com>
2020-01-29 16:30:59 +0200

    update package.py to listen on / only on 3bot.org domain

--- package.py
+++ package.py
@@ -1,22 +1,28 @@
 from Jumpscale import j
 
+
 class Package(j.baseclasses.threebot_package):
     """
     JSX> cl = j.servers.threebot.local_start_zerobot(background=False)
     JSX> cl = j.clients.gedis.get("abc", port=8901, package_name="zerobot.packagemanager")
     JSX> cl.actors.package_manager.package_add(git_url="https://github.com/threefoldtech/www_3bot_org/tree/3bot")
     """
+    DOMAIN = "3bot.org"
     def start(self):
         server = self.openresty
         server.configure()
-        for port in [80, 443]:
-            website = server.get_from_port(port)
-            website.domain = "3bot.org"
-            locations = website.locations.get(f"3bot_locations_{port}")
+        website_3bot = server.websites.get("www_3bot_org")
+        website_3bot.domain = self.DOMAIN
+        website_3bot.port = 80
+        website_3bot.ssl = False
+
+        websites = [server.get_from_port(80), server.get_from_port(443), website_3bot]
+        for website in websites:
+            locations = website.locations.get(f"3bot_locations_{website.name}")
 
             website_location = locations.locations_static.new()
             website_location.name = "3botwebsite"
-            website_location.path_url = "/"
+            website_location.path_url = "/" if website.domain == self.DOMAIN else "/3bot_org"
             fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
             website_location.path_location = fullpath
 
